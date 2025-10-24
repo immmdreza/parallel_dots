9f631c96bd4368f5bb0058d997c03f19ceae058d
immmdreza (Zahra) <ir310022@gmail.com>
2025-10-24 16:54:34 +0330

    creating world.

--- src/world.rs
+++ src/world.rs
@@ -1,10 +1,12 @@
 use std::{
     any::{Any, TypeId},
-    collections::HashMap,
+    collections::{HashMap, HashSet},
+    marker::PhantomData,
 };
 
 pub struct Entity {
     id: usize,
+    component_id: Option<usize>,
     inner: Box<dyn Any>,
 }
 
@@ -15,20 +17,86 @@ impl Entity {
     {
         Self {
             id,
+            component_id: None,
             inner: Box::new(inner),
+        }
+    }
+
+    fn new_composite<T>(id: usize, component_id: usize, inner: T) -> Self
+    where
+        T: Any + 'static,
+    {
+        Self {
+            id,
+            component_id: Some(component_id),
+            inner: Box::new(inner),
+        }
+    }
+
+    pub fn id(&self) -> usize {
+        self.id
+    }
+}
+
+pub struct TypedEntity<'a, T: Any, TEntity> {
+    _phantom: PhantomData<&'a T>,
+    entity: TEntity,
+}
+
+impl<'a, T: Any, TEntity> TypedEntity<'a, T, TEntity> {
+    fn new(entity: TEntity) -> Self {
+        Self {
+            _phantom: PhantomData,
+            entity,
+        }
+    }
+}
+
+impl<'a, T: Any> TypedEntity<'a, T, &'a Entity> {
+    pub fn as_ref(&self) -> Option<&'a T> {
+        self.entity.inner.downcast_ref()
+    }
+}
+
+impl<'a, T: Any> TypedEntity<'a, T, &'a mut Entity> {
+    pub fn as_mut(&'a mut self) -> Option<&'a mut T> {
+        self.entity.inner.downcast_mut()
+    }
+}
+
+impl<'a, T: Any> TypedEntity<'a, T, Entity> {
+    pub fn inner(self) -> Option<T> {
+        self.entity.inner.downcast().ok().map(|e| *e)
+    }
+}
+
+pub struct Component {
+    id: usize,
+    entities: HashSet<usize>,
+}
+
+impl Component {
+    fn new(id: usize, entities: impl IntoIterator<Item = usize>) -> Self {
+        Self {
+            id,
+            entities: entities.into_iter().collect(),
         }
     }
 }
 
 pub struct World {
-    id_counter: usize,
+    component_id_counter: usize,
+    entity_id_counter: usize,
+    components: HashMap<usize, Component>,
     entities: HashMap<TypeId, HashMap<usize, Entity>>,
 }
 
 impl World {
     pub fn new() -> Self {
         Self {
-            id_counter: 1,
+            entity_id_counter: 1,
+            component_id_counter: 1,
+            components: HashMap::new(),
             entities: HashMap::new(),
         }
     }
@@ -37,20 +105,65 @@ impl World {
     where
         T: Any + 'static,
     {
-        let id = self.generate_new_id();
+        let id = self.generate_new_entity_id();
 
         let category = self.entities.entry(TypeId::of::<T>());
         category.or_default().insert(id, Entity::new(id, val));
         id
     }
 
-    pub fn get_entity<T>(&self, id: &usize) -> Option<&Entity>
+    pub fn insert_composite<T1, T2>(&mut self, val1: T1, val2: T2) -> usize
+    where
+        T1: Any + 'static,
+        T2: Any + 'static,
+    {
+        let id1 = self.generate_new_entity_id();
+        let id2 = self.generate_new_entity_id();
+
+        let comp_id = self.generate_new_component_id();
+
+        self.entities
+            .entry(TypeId::of::<T1>())
+            .or_default()
+            .insert(id1, Entity::new_composite(id1, comp_id, val1));
+        self.entities
+            .entry(TypeId::of::<T2>())
+            .or_default()
+            .insert(id2, Entity::new_composite(id2, comp_id, val2));
+
+        self.components
+            .insert(comp_id, Component::new(comp_id, [id1, id2]));
+
+        comp_id
+    }
+
+    pub fn insert_to_component<T>(&mut self, component_id: usize, val: T) -> Option<usize>
+    where
+        T: Any + 'static,
+    {
+        if !self.components.contains_key(&component_id) {
+            return None;
+        }
+
+        let id = self.generate_new_entity_id();
+
+        self.entities
+            .entry(TypeId::of::<T>())
+            .or_default()
+            .insert(id, Entity::new_composite(id, component_id, val));
+
+        self.components.get_mut(&component_id)?.entities.insert(id);
+
+        Some(id)
+    }
+
+    pub fn get_entity<'a, T>(&'a self, id: &usize) -> Option<TypedEntity<'a, T, &'a Entity>>
     where
         T: Any + 'static,
     {
         self.entities
             .get(&TypeId::of::<T>())
-            .map(|e| e.get(id))
+            .map(|e| e.get(id).map(|entity| TypedEntity::new(entity)))
             .flatten()
     }
 
@@ -58,18 +171,19 @@ impl World {
     where
         T: Any + 'static,
     {
-        self.get_entity::<T>(id)
-            .map(|e| e.inner.downcast_ref())
-            .flatten()
+        self.get_entity::<T>(id).map(|e| e.as_ref()).flatten()
     }
 
-    pub fn get_entity_mut<T>(&mut self, id: &usize) -> Option<&mut Entity>
+    pub fn get_entity_mut<'a, T>(
+        &'a mut self,
+        id: &usize,
+    ) -> Option<TypedEntity<'a, T, &'a mut Entity>>
     where
         T: Any + 'static,
     {
         self.entities
             .get_mut(&TypeId::of::<T>())
-            .map(|e| e.get_mut(id))
+            .map(|e| e.get_mut(id).map(|entity| TypedEntity::new(entity)))
             .flatten()
     }
 
@@ -77,31 +191,51 @@ impl World {
     where
         T: Any + 'static,
     {
-        self.get_entity_mut::<T>(id)
-            .map(|e| e.inner.downcast_mut())
-            .flatten()
+        self.entities
+            .get_mut(&TypeId::of::<T>())
+            .map(|e| e.get_mut(id).map(|entity| entity.inner.downcast_mut()))
+            .flatten()?
     }
 
-    pub fn remove_entity<T>(&mut self, id: &usize) -> Option<Entity>
+    pub fn remove_entity<T>(&'_ mut self, id: &usize) -> Option<TypedEntity<'_, T, Entity>>
     where
         T: Any + 'static,
     {
-        self.entities.get_mut(&TypeId::of::<T>())?.remove(id)
+        let entity = self
+            .entities
+            .get_mut(&TypeId::of::<T>())?
+            .remove(id)
+            .map(|e| TypedEntity::new(e));
+
+        if let Some(entity) = &entity {
+            if let Some(comp_id) = entity.entity.component_id {
+                let comp = self.components.get_mut(&comp_id).unwrap();
+                comp.entities.remove(&entity.entity.id);
+                if comp.entities.is_empty() {
+                    self.components.remove(&comp_id);
+                }
+            }
+        }
+
+        entity
     }
 
     pub fn remove<T>(&mut self, id: &usize) -> Option<T>
     where
         T: Any + 'static,
     {
-        self.remove_entity::<T>(id)
-            .map(|e| e.inner.downcast().ok())
-            .flatten()
-            .map(|e| *e)
+        self.remove_entity::<T>(id).map(|e| e.inner()).flatten()
     }
 
-    fn generate_new_id(&mut self) -> usize {
-        let current = self.id_counter;
-        self.id_counter += 1;
+    fn generate_new_entity_id(&mut self) -> usize {
+        let current = self.entity_id_counter;
+        self.entity_id_counter += 1;
+        current
+    }
+
+    fn generate_new_component_id(&mut self) -> usize {
+        let current = self.component_id_counter;
+        self.component_id_counter += 1;
         current
     }
 }
@@ -113,6 +247,22 @@ mod tests {
     #[test]
     fn test_name() {
         let mut world = World::new();
-        world.insert("Hello World".to_string());
+        let id = world.insert("Hello World".to_string());
+
+        let t = world.get_mut::<String>(&id).unwrap();
+        t.push_str(" My dear!");
+
+        assert_eq!(
+            Some("Hello World My dear!".to_string()),
+            world.get::<String>(&id).cloned()
+        );
+
+        assert_eq!(
+            Some("Hello World My dear!".to_string()),
+            world.remove::<String>(&id)
+        );
+
+        assert_eq!(2, world.insert("Goodbye World.".to_string()));
+        assert_eq!(None, world.get::<String>(&id).cloned());
     }
 }
